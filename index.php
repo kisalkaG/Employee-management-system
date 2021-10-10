36f4f1804834f6684621de59774d8083bf70ee8e
kisalkaG <65104308+kisalkaG@users.noreply.github.com>
2021-10-10 19:28:54 +0530

    LogIn Form

--- index.php
+++ index.php
@@ -1,7 +1,59 @@
-<?php 
+<!DOCTYPE html>
+<html>
 
-include './DB/dbConnection.php';
-$conn = OpenCon();
-include './DB/migrations.php';
+<head>
+    <title>PHP Cookies based login system</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
 
-echo "Hello";
+<body>
+    <div class="container">
+        <br>
+        <h2>Login page</h2>
+        <br>
+        <?php
+        include './DB/dbConnection.php';
+        $conn = OpenCon();
+        include './DB/migrations.php';
+        ?>
+
+        <p>haven't account yet? <a href="registration.php">Register here</a></p>
+
+        <form action="loginController.php" method="POST">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" name="email" placeholder="Enter email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="Password" id="password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="rememberMe">
+                <label class="form-check-label" for="exampleCheck1">Remember me</label>
+            </div>
+            <br>
+            <input type="submit" class="btn btn-primary" value="Login" name="login">
+        </form>
+    </div>
+    </form>
+
+    <!-- <?php
+
+    if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
+        $email = $_COOKIE['email'];
+        $password = $_COOKIE['password'];
+
+        echo "<script>
+      document.getElementById('email').value = '$email';
+      document.getElementById('password').value = '$password';   
+    </script>";
+    };
+    ?> -->
+
+</html>
